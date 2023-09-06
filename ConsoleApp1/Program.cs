b4992de167857774442a23e31833b5d6eb760c2d
OlenaM22 <143819929+OlenaM22@users.noreply.github.com>
2023-09-06 17:19:34 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+
+
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 5.0;
+            double result = -6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15;
+            Console.WriteLine("Result of -6*x^3+5*x^2-10*x+15 for x=5.0: " + result);
+        }
+    }
+}
